581b1bf18ad7d97cd710998ff46dd63a848df664
fabcodingzest <fabrizvi786@gmail.com>
2020-06-10 23:57:37 +0530

    Day 8 blog 100daysofcode commit 1

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+const wrapper = document.querySelector('.wrapper');
 const settingIcon = document.querySelectorAll('.settings-icon');
 const settings = document.querySelector('.settings');
 const settingsBg = document.querySelector('.settings__bg');
-const wrapper = document.querySelector('.wrapper');
+const button = document.querySelector('.btn');
+const sliders = document.querySelectorAll('input[type="range"]');
+const sessionSlider = document.querySelector('#session');
+const breakSlider = document.querySelector('#break');
 
 // Handle Settings CLick menu
 function handleSettings () {
@@ -11,4 +15,18 @@ function handleSettings () {
   // })
 }
 
+// handle reset button
+function handleReset () {
+  sessionSlider.value = '25';
+  breakSlider.value = '5';
+}
+
+function handleSliderChange () {
+  
+}
+
 settingIcon.forEach(settingsIcon => settingsIcon.addEventListener('click', handleSettings))
+button.addEventListener('click', handleReset);
+sliders.forEach(slider => {
+  slider.addEventListener('change', handleSliderChange);
+})
