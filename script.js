91f692bc228c5b391517f76109bb0c3608f732b6
fabcodingzest <fabrizvi786@gmail.com>
2020-06-11 17:32:43 +0530

    Made sliders work

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const settingIcon = document.querySelectorAll('.settings-icon');
 const settings = document.querySelector('.settings');
 const settingsBg = document.querySelector('.settings__bg');
 const button = document.querySelector('.btn');
-const sliders = document.querySelectorAll('input[type="range"]');
+const sessionVal = document.querySelector('.session-value');
+const breakVal = document.querySelector('.break-value');
 const sessionSlider = document.querySelector('#session');
 const breakSlider = document.querySelector('#break');
 
@@ -11,22 +12,25 @@ const breakSlider = document.querySelector('#break');
 function handleSettings () {
   settingsBg.classList.toggle("open");
   // wrapper.addEventListener('click', function () {
-  //   // settings.classList.remove("open")
+    // settings.classList.remove("open")
   // })
 }
 
 // handle reset button
 function handleReset () {
   sessionSlider.value = '25';
+  sessionVal.textContent = '25';
+  breakVal.textContent = '5';
   breakSlider.value = '5';
 }
 
-function handleSliderChange () {
+function handleSliderChange (val, element) {
+  console.log(val, element);
+  element.textContent = val;
   
 }
 
 settingIcon.forEach(settingsIcon => settingsIcon.addEventListener('click', handleSettings))
 button.addEventListener('click', handleReset);
-sliders.forEach(slider => {
-  slider.addEventListener('change', handleSliderChange);
-})
+sessionSlider.addEventListener('change', (e) => handleSliderChange(e.currentTarget.value, sessionVal));
+breakSlider.addEventListener('change', (e) => handleSliderChange(e.currentTarget.value, breakVal));
