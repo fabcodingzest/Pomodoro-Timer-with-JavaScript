82d24471badd98a20c16ab5ae4fd26543ff679dc
fabcodingzest <fabrizvi786@gmail.com>
2020-06-11 23:04:38 +0530

    Made countdown with play and pause button work

--- script.js
+++ script.js
@@ -7,13 +7,40 @@ const sessionVal = document.querySelector('.session-value');
 const breakVal = document.querySelector('.break-value');
 const sessionSlider = document.querySelector('#session');
 const breakSlider = document.querySelector('#break');
+const timerDisplay = document.querySelector('.timer');
+const play = document.querySelector('.play');
+const pause = document.querySelector('.pause');
+
+timerDisplay.textContent = '25:00'
+let countdown;
+
+function timer (seconds) {
+  clearInterval(countdown);
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // check if we should stop
+    if (secondsLeft <= 0) {
+      clearInterval(countdown);
+      return;
+    }
+    // display it
+    displayTimeLeft(secondsLeft);
+  }, 1000)
+}
+
+function displayTimeLeft (seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes < 10 ? '0': ''}${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  timerDisplay.textContent = display;
+}
+
 
 // Handle Settings CLick menu
 function handleSettings () {
   settingsBg.classList.toggle("open");
-  // wrapper.addEventListener('click', function () {
-    // settings.classList.remove("open")
-  // })
 }
 
 // handle reset button
@@ -25,12 +52,22 @@ function handleReset () {
 }
 
 function handleSliderChange (val, element) {
-  console.log(val, element);
   element.textContent = val;
-  
 }
 
+function handlePlay () {
+  console.log(sessionVal);
+  timer(sessionSlider.value*60);
+}
+
+function handlePause () {
+  let pauseTime = timerDisplay.textContent;
+  console.log(pauseTime);
+  clearInterval(countdown);
+}
 settingIcon.forEach(settingsIcon => settingsIcon.addEventListener('click', handleSettings))
 button.addEventListener('click', handleReset);
 sessionSlider.addEventListener('change', (e) => handleSliderChange(e.currentTarget.value, sessionVal));
 breakSlider.addEventListener('change', (e) => handleSliderChange(e.currentTarget.value, breakVal));
+play.addEventListener('click', handlePlay);
+pause.addEventListener('click', handlePause);
